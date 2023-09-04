49142b1067e2d59952eab0f5c3b9177960b4b650
David Li <fantasticmrdavid@gmail.com>
2023-09-04 20:10:12 +1000

    Combine uploader into main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import boto3
 import json
 import time
 
@@ -8,6 +9,18 @@ from pydexcom import Dexcom
 username = os.environ["DEXCOM_USERNAME"]
 password = os.environ["DEXCOM_PASSWORD"]
 dexcom = Dexcom(username, password, True)
+
+access_key_id = os.environ["AWS_S3_ACCESS_KEY"]
+secret_key = os.environ["AWS_S3_SECRET"]
+s3bucket = os.environ["AWS_S3_BUCKET"]
+
+session = boto3.Session(
+    aws_access_key_id=access_key_id,
+    aws_secret_access_key=secret_key,
+)
+
+s3 = session.resource("s3")
+bucket = s3.Bucket(s3bucket)
 
 while True:
     glucose_reading = dexcom.get_current_glucose_reading()
@@ -24,4 +37,5 @@ while True:
     with open('readings.json', 'w') as outfile:
         json.dump(writeOutput, outfile)
     print(display)
+    bucket.upload_file('readings.json', 'readings.json', ExtraArgs={'ACL':'public-read'})
     time.sleep(300)
--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-npx json-server --watch readings.json
+python3 main.py &
+python3 uploader.py &
+npx json-server --watch readings.json -p 3001
